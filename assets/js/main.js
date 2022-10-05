f658429dcbc584f97a700904d21b014c621d70d5
emanmoq <42995029+emanmoq@users.noreply.github.com>
2022-10-05 01:25:51 -0700

    upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,27 @@
-$(document).ready(function() {
+$(document).ready(function () {
     'use strict';
+    /*-----------------------------------------------------------------------------------*/
+    $(".workSlider").owlCarousel({
+        nav: true,
+        loop: false,
+        dots: true,
+        items: 5,
+        navText: ['<i class="fa fa-chevron-left"></i>', '<i class="fa fa-chevron-right"></i>'],
+        margin: 20,
+        responsive: {
+          0: {
+            items: 1
+          },
+          700: {
+            items: 2
+          },
+          1000: {
+            items: 5,
+          },
+    
+        }
+      });
+
     /*-----------------------------------------------------------------------------------*/
     /*	STICKY HEADER
     /*-----------------------------------------------------------------------------------*/
@@ -12,30 +34,31 @@ $(document).ready(function() {
                 stick: 'banner--stick',
                 unstick: 'banner--unstick'
             },
-            onStick: function() {
+            onStick: function () {
                 $($.SmartMenus.Bootstrap.init);
             },
-            onUnstick: function() {
+            onUnstick: function () {
                 $('.navbar .btn-group').removeClass('open');
             }
         };
         var banner = new Headhesive('.navbar', options);
     }
 });
-    /*-----------------------------------------------------------------------------------*/
-    /*	Slider
-    /*-----------------------------------------------------------------------------------*/
+/*-----------------------------------------------------------------------------------*/
+/*	Slider
+/*-----------------------------------------------------------------------------------*/
 
 $('#slider').revolution({
     sliderType: "standard",
-    jsFileLocation: "include/rs-plugin/js/",
+    jsFileLocation: "asesets/revolution/js/",
     sliderLayout: "fullscreen",
+    fullScreenOffsetContainer: ".navbar:not(.fixed)",
     dottedOverlay: "none",
     delay: 9000,
     responsiveLevels: [1140, 1024, 778, 480],
     visibilityLevels: [1140, 1024, 778, 480],
-    gridwidth:[1140, 1024, 778, 480],
-    responsiveLevels: [1240, 1024, 778, 480],
+    gridwidth: [1140, 1024, 778, 480],
+    gridheight: [728, 768, 960, 720],
     lazyType: "none",
     shadow: 0,
     spinner: "off",
@@ -43,52 +66,79 @@ $('#slider').revolution({
     stopAfterLoops: 0,
     stopAtSlide: 1,
     shuffle: "off",
-    autoHeight: "off",
-    fullScreenAutoWidth: "off",
-    fullScreenAlignForce: "off",
-    fullScreenOffsetContainer: "",
-    fullScreenOffset: "",
-    disableProgressBar: "on",
     hideThumbsOnMobile: "off",
     hideSliderAtLimit: 0,
     hideCaptionAtLimit: 0,
     hideAllCaptionAtLilmit: 0,
     debugMode: false,
     fallbacks: {
-        simplifyAll:"off",
-        disableFocusListener:false,
+        simplifyAll: "off",
+        disableFocusListener: false,
     },
     parallax: {
-        type:"mouse",
-        origo:"slidercenter",
-        speed:1000,
-        levels:[2,4,6,8,10,12,14,16,18,20,22,24,49,50,51,55],
+        type: "mouse",
+        origo: "slidercenter",
+        speed: 1000,
+        levels: [1, 2, 4, 6, 8, 10, 12, 16, 18, 20, 22, 24, 49, 50, 51, 55],
     },
 });
-    /*-----------------------------------------------------------------------------------*/
-	/*	ONEPAGE SMOOTH SCROLL
-	/*-----------------------------------------------------------------------------------*/	
-	$(function() {
-        setTimeout(function() {
-          if (location.hash) {
+/*-----------------------------------------------------------------------------------*/
+/*	ONEPAGE SMOOTH SCROLL
+/*-----------------------------------------------------------------------------------*/
+$(function () {
+    setTimeout(function () {
+        if (location.hash) {
             window.scrollTo(0, 0);
             var target = location.hash.split('#');
-            smoothScrollTo($('#'+target[1]));
-          }
-        }, 1);  
-        $('a.scroll[href*=#]:not([href=#])').on('click', function() {
-          if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            smoothScrollTo($('#' + target[1]));
+        }
+    }, 1);
+    $('a.scroll[href*=#]:not([href=#])').on('click', function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
             smoothScrollTo($(this.hash));
             return false;
-          }
-        });  
-        function smoothScrollTo(target) {
-          var target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-          
-          if (target.length) {
-            $('html,body').animate({
-              scrollTop: target.offset().top
-            }, 1500, 'easeInOutExpo');
-          }
         }
-      });
+    });
+    function smoothScrollTo(target) {
+        var target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+
+        if (target.length) {
+            $('html,body').animate({
+                scrollTop: target.offset().top
+            }, 1500, 'easeInOutExpo');
+        }
+    }
+    new WOW().init();
+    /*-----------------------------------------------------------------------------------*/
+    /*	GO TO TOP
+    /*-----------------------------------------------------------------------------------*/
+    $.scrollUp({
+        scrollName: 'scrollUp',
+        // Element ID
+        scrollDistance: 300,
+        // Distance from top/bottom before showing element (px)
+        scrollFrom: 'top',
+        // 'top' or 'bottom'
+        scrollSpeed: 300,
+        // Speed back to top (ms)
+        easingType: 'linear',
+        // Scroll to top easing (see http://easings.net/)
+        animation: 'fade',
+        // Fade, slide, none
+        animationInSpeed: 200,
+        // Animation in speed (ms)
+        animationOutSpeed: 200,
+        // Animation out speed (ms)
+        scrollText: '<span class="btn btn-square btn-full-rounded btn-icon"><i class="fa fa-chevron-up"></i></span>',
+        // Text for element, can contain HTML
+        scrollTitle: false,
+        // Set a custom <a> title if required. Defaults to scrollText
+        scrollImg: false,
+        // Set true to use image
+        activeOverlay: false,
+        // Set CSS color to display scrollUp active point, e.g '#00FFFF'
+        zIndex: 1001 // Z-Index for the overlay
+    });
+
+
+});
